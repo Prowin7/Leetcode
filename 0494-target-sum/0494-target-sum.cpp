e3ee08e8ca7687807daa7fdcbfb2b8fb113dd771
Prowin7 <iec2022118@iiita.ac.in>
2025-07-20 00:08:05 +0530

    Time: 15 ms (53.93%), Space: 26.4 MB (24.09%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -3,12 +3,18 @@ class Solution {
         if(i==nums.size()){
             return sum==target;
         }
+        if(dp[i][sum+offset]!=-1) return dp[i][sum+offset];
         int plus=func(i+1,sum-nums[i],nums,target);
         int minus=func(i+1,sum+nums[i],nums,target);
-        return plus+minus;
+        return dp[i][sum+offset]=plus+minus;
     }
+
 public: 
+vector<vector<int>> dp;
+int offset;
     int findTargetSumWays(vector<int>& nums, int target) {
+        offset=accumulate(nums.begin(),nums.end(),0);
+        dp=vector<vector<int>>(nums.size()+1,vector<int>(2*offset+1,-1));
         return func(0,0,nums,target);
     }
 };
