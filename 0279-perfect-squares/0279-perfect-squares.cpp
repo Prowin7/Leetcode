80ef018a6fe3b6687202b16ce4e8b0b5506cf99f
Prowin7 <iec2022118@iiita.ac.in>
2025-07-21 16:16:08 +0530

    Time: 73 ms (45.88%), Space: 14.1 MB (26.13%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,18 +1,25 @@
 class Solution {
-public:
-int func(int n,vector<int> &dp){
-    if(n==0) return 0;
-    if(dp[n]!=-1) return dp[n];
-    int ans=n;
-    for(int i=1;i*i<=n;i++){
-        int square=i*i;
-        ans=min(ans,1+func(n-square,dp));
-    }
-    return dp[n]=ans;
-}
-    int numSquares(int n) {
+    vector<int> dp;
 
-        vector<int> dp(n+1,-1);
-        return func(n,dp);
+    int tryAllSquares(int i, int rem) {
+        if (i * i > rem) return INT_MAX;
+        int res = func(rem - i * i);
+        int pick = (res == INT_MAX) ? INT_MAX : 1 + res;
+        int notPick = tryAllSquares(i + 1, rem);
+        return min(pick, notPick);
+    }
+
+    int func(int rem) {
+        if (rem == 0) return 0;
+        if (rem < 0) return INT_MAX;
+        if (dp[rem] != -1) return dp[rem];
+
+        return dp[rem] = tryAllSquares(1, rem);
+    }
+
+public:
+    int numSquares(int n) {
+        dp = vector<int>(n + 1, -1);
+        return func(n);
     }
 };
