68825ebd921b45a41f923dfc7e40d8bb3b987ae2
Prowin7 <iec2022118@iiita.ac.in>
2025-07-25 14:44:07 +0530

    Time: 0 ms (100%), Space: 22.6 MB (13.03%) - LeetHub

---
+++ 0275-h-index-ii/0275-h-index-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int hIndex(vector<int>& nums) {
+        int n=nums.size();
+        int left=0;
+        int right=n;
+        
+        while(left<right){
+           int mid=left+(right-left)/2;
+            if(nums[mid]>=n-mid){
+                right=mid;
+            }
+            else{
+                left=mid+1;
+            }
+
+        }
+        return n-left;
+    }
+};
