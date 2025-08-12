027b2d0796aea5d5f326d20fab74cdaa620a2e65
Prowin7 <iec2022118@iiita.ac.in>
2025-08-12 12:48:44 +0530

    Time: 0 ms (100%), Space: 11.8 MB (6.67%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int n=nums.size();
+        int i=0;
+        int j=0;
+        int cnt=0;
+        while(j<n){
+            if(nums[j]!=val){
+                swap(nums[i],nums[j]);
+                i++;
+            }
+            cnt++;
+            j++;
+        }
+        return i;
+    }
+};
