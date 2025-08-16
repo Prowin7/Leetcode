420ecf8078da1d4d4f871489a810663be2c0c8ab
Prowin7 <iec2022118@iiita.ac.in>
2025-08-16 11:54:57 +0530

    Time: 1 ms (52.29%), Space: 19.2 MB (92.79%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,30 +1,47 @@
-class Solution {
-    void dfs(int node,vector<int> &vis,vector<vector<int>> &adj){
-        vis[node]=1;
-        for(auto neighbor:adj[node]){
-            if(!vis[neighbor]){
-                dfs(neighbor,vis,adj);
-            }
+class DisjointSet {
+    vector<int> rank, parent;
+
+public:
+    DisjointSet(int n) {
+        parent.resize(n + 1);
+        rank.resize(n + 1, 0);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
         }
     }
+    int find(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = find(parent[node]);
+    }
+    void unionRank(int u, int v) {
+        int par_u = find(u);
+        int par_v = find(v);
+        if (rank[par_u] < rank[par_v]) {
+            parent[par_u] = par_v;
+        } else if (rank[par_v] < rank[par_u]) {
+            parent[par_v] = par_u;
+        } else {
+            parent[par_v] = par_u;
+            rank[par_u]++;
+        }
+    }
+};
+class Solution {
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int n=isConnected.size();
-        int m=isConnected[0].size();
-        vector<vector<int>> adj(n);
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if (isConnected[i][j] == 1 && i != j)
-                adj[i].push_back(j);
+        int n = isConnected.size();
+        DisjointSet ds(n);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (isConnected[i][j] == 1 && i != j) {
+                    ds.unionRank(i,j);
+                }
             }
         }
         int cnt=0;
-        vector<int> vis(n,0);
         for(int i=0;i<n;i++){
-            if(!vis[i]){
-                cnt++;
-                dfs(i,vis,adj);
-            }
+            if(ds.find(i)==i) cnt++;
         }
         return cnt;
     }
