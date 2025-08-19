7be318e9446d9cc00912996a84c52a080940a5eb
Prowin7 <iec2022118@iiita.ac.in>
2025-08-19 14:32:18 +0530

    Time: 253 ms (16.63%), Space: 197.7 MB (13.33%) - LeetHub

--- 1300-critical-connections-in-a-network/1300-critical-connections-in-a-network.cpp
+++ 1300-critical-connections-in-a-network/1300-critical-connections-in-a-network.cpp
@@ -1,38 +1,39 @@
 class Solution {
-    vector<vector<int>> bridges;
-    int timer=1;
-    vector<int> tin;
-    vector<int> low;
-    void dfs(int node,int parent,vector<int>&vis,vector<vector<int>>&adj){
-        vis[node]=1;
-        tin[node]=timer;
-        low[node]=timer;
-        timer++;
-        for(auto neigh:adj[node]){
-            if(neigh==parent) continue;
-            if(!vis[neigh]){
-                dfs(neigh,node,vis,adj);
-                low[node]=min(low[node],low[neigh]);
-                if(low[neigh]>tin[node]){
-                    bridges.push_back({neigh,node});
-                }
-            }
-            else {
-                low[node]=min(low[node],low[neigh]);
+int timer=1;
+vector<int> tin;
+vector<int> low;
+vector<vector<int>> bridges;
+void dfs(int node,int parent,vector<int>&vis,vector<vector<int>>&adj){
+    vis[node]=1;
+    tin[node]=low[node]=timer;
+    timer++;
+    for(auto neigh:adj[node]){
+        if(neigh==parent) continue;
+        if(!vis[neigh]){
+            dfs(neigh,node,vis,adj);
+            low[node]=min(low[neigh],low[node]);
+            if(low[neigh]>tin[node]){
+                bridges.push_back({node,neigh});
             }
         }
+        else{
+            low[node]=min(low[node],low[neigh]);
+        }
     }
+}
 public:
     vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
         vector<vector<int>> adj(n);
         for(auto it:connections){
-            adj[it[0]].push_back(it[1]);
-            adj[it[1]].push_back(it[0]);
-        }
+            int a=it[0];
+            int b=it[1];
+            adj[a].push_back(b);
+            adj[b].push_back(a);
+        }    
+        tin.resize(n,0);
+        low.resize(n,0);
         vector<int> vis(n,0);
-        tin=vector<int>(n,0);
-        low=vector<int>(n,0);
-        dfs(0,-1,vis,adj);
-        return bridges;
+        dfs(0,-1,vis,adj); 
+        return bridges; 
     }
 };
