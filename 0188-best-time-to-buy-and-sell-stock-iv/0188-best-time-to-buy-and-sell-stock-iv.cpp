131ae1cef793c942b24eb1b560ccf5abc5e41b52
Prowin7 <iec2022118@iiita.ac.in>
2025-09-20 16:07:25 +0530

    Time: 69 ms (5.29%), Space: 32.4 MB (5.52%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,27 +1,29 @@
 class Solution {
     int n;
     vector<vector<vector<int>>> dp;
-    int func(int day,int k, int canBuy,vector<int>& prices){
-        if(day==n || k==0) return 0;
-        if(dp[day][k][canBuy]!=-1) return dp[day][k][canBuy];
-        int profit=0;
-        if(canBuy){
-            int buy = -prices[day] + func(day+1,k,0,prices); //make 0 
-            int rest = func(day+1,k,1,prices);
-            profit = max(rest,buy);
-        }
-        else{ // sell
-            int sell = prices[day] + func(day+1,k-1,1,prices);           
-            int rest = func(day+1,k,0,prices);
-            profit = max(sell,rest);
-        }
+    int func(int day, int k, int canBuy, vector<int>& prices) {
+        if (day == n || k == 0) return 0;
+        if (dp[day][k][canBuy] != -1) return dp[day][k][canBuy];
         
-        return dp[day][k][canBuy]=profit;
+        int profit = 0;
+        if (canBuy) {
+            int buy = -prices[day] + func(day + 1, k, 0, prices); // make 0
+            int rest = func(day + 1, k, 1, prices);
+            profit = max(rest, buy);
+        } else { // sell
+            int sell = prices[day] + func(day + 1, k - 1, 1, prices);
+            int rest = func(day + 1, k, canBuy, prices);
+            profit = max(sell, rest);
+        }
+
+        return dp[day][k][canBuy] = profit;
     }
+
 public:
     int maxProfit(int k, vector<int>& prices) {
         n = prices.size();
-        dp = vector<vector<vector<int>>>(n+1,vector<vector<int>>(k+1,vector<int>(2,-1)));
-     return func(0,k,1,prices);   
+        dp = vector<vector<vector<int>>>(
+            n + 1, vector<vector<int>>(k + 1, vector<int>(2, -1)));
+        return func(0, k, 1, prices);
     }
 };
