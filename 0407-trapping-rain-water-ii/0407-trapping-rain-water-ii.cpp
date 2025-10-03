54f8ac78ab3fd4d2dfefd71f16310d98543848bd
Prowin7 <iec2022118@iiita.ac.in>
2025-10-03 22:34:18 +0530

    Time: 38 ms (36.75%), Space: 19.1 MB (40.22%) - LeetHub

---
+++ 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int trapRainWater(vector<vector<int>>& h) {
+        int m = h.size(), n = h[0].size();
+        if (m <= 2 || n <= 2) return 0; 
+
+        vector<vector<int>> vis(m, vector<int>(n, 0));
+        priority_queue<array<int,3>, vector<array<int,3>>, greater<>> pq;
+
+        // Push all boundary cells
+        for (int i = 0; i < m; i++) {
+            pq.push({h[i][0], i, 0});
+            pq.push({h[i][n-1], i, n-1});
+            vis[i][0] = vis[i][n-1] = 1;
+        }
+        for (int j = 0; j < n; j++) {
+            pq.push({h[0][j], 0, j});
+            pq.push({h[m-1][j], m-1, j});
+            vis[0][j] = vis[m-1][j] = 1;
+        }
+
+        int water = 0;
+        vector<pair<int,int>> dir = {{0,1},{1,0},{-1,0},{0,-1}};
+
+        while (!pq.empty()) {
+            int ht = pq.top()[0];
+            int x  = pq.top()[1];
+            int y  = pq.top()[2];
+            pq.pop();
+
+            for (auto d:dir) {
+                int nx = x + d.first;
+                int ny = y + d.second;
+
+                if (nx < 0 || ny < 0 || nx >= m || ny >= n || vis[nx][ny]) continue;
+                vis[nx][ny] = 1;
+
+                if (h[nx][ny] < ht) {
+                    water += ht - h[nx][ny];
+                    pq.push({ht, nx, ny});
+                } else {
+                    pq.push({h[nx][ny], nx, ny});
+                }
+            }
+        }
+        return water;
+    }
+};
