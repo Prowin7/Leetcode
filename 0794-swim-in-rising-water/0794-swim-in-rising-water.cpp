1fc7b49ea3a077c854ecc8f784f9ed1cee9d86aa
Prowin7 <iec2022118@iiita.ac.in>
2025-10-04 18:50:26 +0530

    Time: 2 ms (90.75%), Space: 12.7 MB (86.21%) - LeetHub

--- 0794-swim-in-rising-water/0794-swim-in-rising-water.cpp
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.cpp
@@ -3,28 +3,34 @@ public:
     int swimInWater(vector<vector<int>>& grid) {
         int r = grid.size();
         int c = grid[0].size();
-        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>> pq;
-        pq.push({grid[0][0],{0,0}}); // {t,{x,y}}
-        
-        vector<pair<int,int>> dir = {{1,0},{0,1},{-1,0},{0,-1}};
-        vector<vector<int>> vis(r,vector<int>(c,0));
-        while(!pq.empty()){
+        priority_queue<pair<int, pair<int, int>>,
+                       vector<pair<int, pair<int, int>>>,
+                       greater<pair<int, pair<int, int>>>>
+            pq;
+        pq.push({grid[0][0], {0, 0}}); // {t,{x,y}}
+
+        vector<pair<int, int>> dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+        vector<vector<int>> vis(r, vector<int>(c, 0));
+        vis[0][0]=1;
+        while (!pq.empty()) {
             int t = pq.top().first;
             int cx = pq.top().second.first;
             int cy = pq.top().second.second;
-            
-            if(cx==r-1 && cy==c-1) return t;
+
+            if (cx == r - 1 && cy == c - 1)
+                return t;
             pq.pop();
-            
-            for(auto d:dir){
-                int nx = cx+d.first;
-                int ny = cy+d.second;
-                if(nx<0||ny<0||nx>=r||ny>=c||vis[nx][ny]) continue;
-                vis[nx][ny]=1;
-                if(grid[nx][ny] <= t){
-                    pq.push({t,{nx,ny}});
+
+            for (auto d : dir) {
+                int nx = cx + d.first;
+                int ny = cy + d.second;
+                if (nx < 0 || ny < 0 || nx >= r || ny >= c || vis[nx][ny])
+                    continue;
+                vis[nx][ny] = 1;
+                if (grid[nx][ny] <= t) {
+                    pq.push({t, {nx, ny}});
                 } else {
-                    pq.push({grid[nx][ny],{nx,ny}});
+                    pq.push({grid[nx][ny], {nx, ny}});
                 }
             }
         }
