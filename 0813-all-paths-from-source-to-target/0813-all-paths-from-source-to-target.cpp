dbba4d404e63240b396aa9d618328e0656dafb69
Prowin7 <iec2022118@iiita.ac.in>
2025-10-09 15:21:12 +0530

    Time: 6 ms (47.75%), Space: 15.4 MB (42.3%) - LeetHub

--- 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.cpp
+++ 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.cpp
@@ -1,21 +1,30 @@
 class Solution {
     vector<vector<int>> ans;
-    void dfs(int node,vector<int> &path,vector<vector<int>>&graph){
+    int n;
+    vector<int> visited;
+    void backtrack(int node,vector<vector<int>>&graph,vector<int> &path){
         path.push_back(node);
         if(node==graph.size()-1){
             ans.push_back(path);
+            path.pop_back();
+            return ;
         }
-        else{
-            for(int neigh:graph[node]){
-                dfs(neigh,path,graph);
+        visited[node] = true;
+        for(auto neigh:graph[node]){
+            if(!visited[neigh]){
+                backtrack(neigh,graph,path);
             }
         }
+        visited[node]=false;
         path.pop_back();
     }
+
 public:
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        n = graph.size();
+        visited = vector<int>(n, false);
         vector<int> path;
-        dfs(0,path,graph);
+        backtrack(0, graph,path);
         return ans;
     }
 };
