e5c9017c6203074a168a0ddd9c9afb11a5a9dedb
Prowin7 <iec2022118@iiita.ac.in>
2025-10-28 18:44:59 +0530

    Time: 18 ms (53.4%), Space: 45.8 MB (35.22%) - LeetHub

---
+++ 0917-boats-to-save-people/0917-boats-to-save-people.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        int n = people.size();
+        int cnt=0;
+        int sum=0;
+        sort(people.begin(), people.end());
+        int i=0, j=n-1;
+        while(i<=j){
+            if(people[i]+people[j]<=limit){
+                i++;
+                j--;
+                cnt++;
+            } else{
+                j--;
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+};
