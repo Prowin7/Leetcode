e594c1c417a65f3ce61a37e5541d43b464b9f30b
Prowin7 <iec2022118@iiita.ac.in>
2025-11-04 17:39:46 +0530

    Time: 0 ms (100%), Space: 8.5 MB (49.05%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,14 +1,14 @@
 class Solution {
-    vector<int> dp;
+     vector<int> dp;
     int func(int n){
         if(n==0) return 1;
         if(n<0) return 0;
         if(dp[n]!=-1) return dp[n];
-        return dp[n]=func(n-1)+func(n-2);
+        return dp[n] = func(n-1)+func(n-2);
     }
 public:
     int climbStairs(int n) {
-        dp=vector<int>(n+1,-1);
+       dp = vector<int>(n+1,-1);
         return func(n);
     }
 };
