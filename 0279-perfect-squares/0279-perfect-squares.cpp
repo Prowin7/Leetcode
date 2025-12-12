d451fb7fc1b22f8d39e2b8b8a22d6bd8b303981f
Prowin7 <iec2022118@iiita.ac.in>
2025-12-12 17:29:48 +0530

    Time: 75 ms (38.42%), Space: 14 MB (29.63%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -4,15 +4,16 @@ class Solution {
         if(n==0) return 0;
         if(n<0) return INT_MAX;
         if(dp[n]!=-1) return dp[n];
-        int ans=n;
+        int ans = n;
         for(int i=1;i*i<=n;i++){
-            ans=min(ans,1+func(n-i*i));
+            ans = min(ans, 1+func(n-i*i));
         }
         return dp[n] = ans;
     }
 public:
+
     int numSquares(int n) {
-        dp=vector<int> (n+1,-1);
+        dp = vector<int>(n+1,-1);
         return func(n);
     }
 };
