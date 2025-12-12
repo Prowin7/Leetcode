e0696723c15844d94d1d4aa1f804da9cb866decf
Prowin7 <iec2022118@iiita.ac.in>
2025-12-12 18:01:30 +0530

    Time: 0 ms (100%), Space: 10.3 MB (74.72%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,17 +1,21 @@
 class Solution {
-    int func(int s,int e,vector<int> & nums,vector<int> &dp){
-        if(s>e) return 0;
-        if(dp[s]!=-1) return dp[s];
-        int take=nums[s]+func(s+2,e,nums,dp);
-        int nottake=func(s+1,e,nums,dp);
-        return dp[s] = max(take,nottake);
+    int func(int left, int right, vector<int>& nums){
+        int prev1=0, prev2=0;
+        for(int i =left;i<=right;i++){
+            int take = nums[i] + prev2;
+            int skip = prev1;
+            int cur=max(take,skip);
+            prev2=prev1;
+            prev1=cur;
+        }
+        return prev1;
     }
 public:
     int rob(vector<int>& nums) {
-        if(nums.size()==1) return nums[0];
-        if(nums.size()==2) return max(nums[0],nums[1]);
-        vector<int> dp1(nums.size()+1,-1);
-        vector<int> dp2(nums.size()+1,-1);
-        return max(func(0,nums.size()-2,nums,dp1),func(1,nums.size()-1,nums,dp2));
+        int n = nums.size();
+        if(n==0) return 0;
+        if(n==1) return nums[0];
+        if(n==2) return max(nums[0],nums[1]);
+        return max(func(0,n-2,nums), func(1,n-1,nums));
     }
 };
