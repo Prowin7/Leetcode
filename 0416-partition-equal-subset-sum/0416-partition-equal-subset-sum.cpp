634801df6ae3db9b98dc120bfd304324e161c584
Prowin7 <iec2022118@iiita.ac.in>
2025-12-29 15:33:42 +0530

    Time: 39 ms (94.32%), Space: 14.8 MB (76.54%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
-vector<vector<int>> dp;
-bool func(int i,int sum,vector<int>& nums){
-    if(sum==0) return true;
-    if(i==nums.size()) return false;
-    if(dp[i][sum]!=-1) return dp[i][sum];
-    bool pick=false;
-    if(nums[i]<=sum)
-    pick=func(i+1,sum-nums[i],nums);
-    bool notPick=func(i+1,sum,nums);
-    return dp[i][sum] = pick||notPick;
-}
     bool canPartition(vector<int>& nums) {
-        int n=nums.size();
-        int sum=0;
-        for(int i:nums) sum+=i;
-        if(sum%2==1) return false;
-        dp=vector<vector<int>>(n+1,vector<int> (sum/2+1,-1));
-        return func(0,sum/2,nums);
+        int sum = 0;
+        for (auto it : nums) sum += it;
+
+        if (sum % 2 != 0) return false;
+        sum /= 2;
+
+        vector<int> dp(sum + 1, 0);
+        dp[0] = 1;   // FIX: base case
+
+        for (int i = 0; i < nums.size(); i++) {
+            for (int s = sum; s >= nums[i]; s--) {
+                dp[s] = dp[s] || dp[s - nums[i]];
+            }
+        }
+        return dp[sum];
     }
 };
