b6250c232c552e78a8a2ee9a8a9e44ef06cc1f44
Prowin7 <iec2022118@iiita.ac.in>
2025-12-31 17:51:13 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,18 +1,14 @@
 class Solution {
-    int func(int i,int left,vector<int> &coins){
-        if(left==0) return 1;
-        if(i>=coins.size()) return 0;
-        if(left<0) return 0;
-        if(dp[i][left]!=-1) return dp[i][left];
-        int pick=func(i,left-coins[i],coins);
-        int notPick=func(i+1,left,coins);
-        return dp[i][left]=pick+notPick;
-    }
 public:
-vector<vector<int>> dp;
     int change(int amount, vector<int>& coins) {
-        int n=coins.size();
-        dp=vector<vector<int>>(n+1,vector<int>(amount+1,-1));
-        return func(0,amount,coins);
+        int n = coins.size();
+        vector<long long> dp(amount+1,0);
+        dp[0]=1;
+        for(int i=0;i<n;i++){
+            for(int c=coins[i];c<=amount;c++){
+                dp[c] = dp[c]+dp[c-coins[i]];
+            }
+        }
+        return dp[amount];
     }
 };
