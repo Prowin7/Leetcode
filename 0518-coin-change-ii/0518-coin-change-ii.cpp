3de6bf21cab87c15bef89feb4e89f16ad7d6ebbd
Prowin7 <iec2022118@iiita.ac.in>
2025-12-31 17:54:42 +0530

    Time: 11 ms (74.34%), Space: 10.3 MB (85.32%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -2,13 +2,18 @@ class Solution {
 public:
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<long long> dp(amount+1,0);
-        dp[0]=1;
-        for(int i=0;i<n;i++){
-            for(int c=coins[i];c<=amount;c++){
-                dp[c] = dp[c]+dp[c-coins[i]];
+        vector<long long> dp(amount + 1, 0);
+
+        dp[0] = 1;
+
+        for (int i = 0; i < n; i++) {
+            for (int c = coins[i]; c <= amount; c++) {
+                if (dp[c] <= LLONG_MAX - dp[c - coins[i]]) {
+                    dp[c] += dp[c - coins[i]];
+                }
             }
         }
-        return dp[amount];
+
+        return (int)dp[amount];
     }
 };
