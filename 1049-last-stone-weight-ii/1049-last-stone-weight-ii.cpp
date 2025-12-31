80fd0faa349b7a0801b163d0835054341219c74d
Prowin7 <iec2022118@iiita.ac.in>
2025-12-31 13:26:07 +0530

    Time: 0 ms (100%), Space: 11.7 MB (49.71%) - LeetHub

--- 1049-last-stone-weight-ii/1049-last-stone-weight-ii.cpp
+++ 1049-last-stone-weight-ii/1049-last-stone-weight-ii.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     int lastStoneWeightII(vector<int>& stones) {
+        int sum = 0;
+        for(auto it:stones) sum+=it;
         int n = stones.size();
-        int total = 0;
-        for(auto it:stones) total+=it;
-        vector<int> dp(total+1,0);
+        vector<int> dp(sum+1,0);
         dp[0]=1;
         for(int i=0;i<n;i++){
-            for(int s = total ;s>=stones[i];s--){
+            for(int s = sum ; s>=stones[i];s--){
                 dp[s] = dp[s]||dp[s-stones[i]];
             }
         }
         int ans = INT_MAX;
-        for(int i=0;i<=total/2;i++){
+        for(int i=0;i<=sum/2;i++){
             if(dp[i])
-            ans=min(ans,total-2*i);
+            ans=min(ans,sum-2*i);
         }
         return ans;
     }
