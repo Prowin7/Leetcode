41be5ac580791327a28ced0ccd230fa3a0bde21c
Prowin7 <iec2022118@iiita.ac.in>
2025-12-31 18:08:31 +0530

    Time: 2 ms (42.99%), Space: 9.1 MB (73.6%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -1,24 +1,16 @@
 class Solution {
-    vector<vector<int>> dp;
-    int func(int i, int target, vector<int>& nums) {
-        if (target == 0) return 1;        // valid sequence
-        if (i >= nums.size()) return 0;   // no more choices
-        if(dp[i][target]!=-1) return dp[i][target];
-
-        int pick = 0;
-        if (target - nums[i] >= 0) {
-            // restart from 0 because order matters
-            pick = func(0, target - nums[i], nums);
-        }
-
-        int notPick = func(i + 1, target, nums);
-
-        return dp[i][target] = pick + notPick;
-    }
 public:
     int combinationSum4(vector<int>& nums, int target) {
-        int n=nums.size();
-        dp=vector<vector<int>>(n,vector<int>(target+1,-1));
-        return func(0, target, nums);
+        vector<long long> dp(target + 1, 0);
+        dp[0] = 1;
+
+        for (int j = 1; j <= target; j++) {
+            for (int i = 0; i < nums.size(); i++) {
+                if (j >= nums[i] && dp[j] <= LLONG_MAX - dp[j - nums[i]]) {
+                    dp[j] += dp[j - nums[i]];
+                }
+            }
+        }
+        return (int)dp[target];
     }
 };
