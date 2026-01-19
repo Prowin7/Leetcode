7357279c079ab45c01f016267597d96a10f83413
Prowin7 <iec2022118@iiita.ac.in>
2026-01-19 23:09:46 +0530

    Time: 0 ms (100%), Space: 8.2 MB (95.7%) - LeetHub

---
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        int n = s.size();
+        int left = 0;
+        int right = 0;
+        for(int i=0;i<n;i++){
+            if(s[i]=='('){
+                right++;
+            }
+            else if(s[i]==')'&&right>0){
+                right--;
+            }
+            else {
+                left++;
+            }
+        }
+        return left+right;
+    }
+};
