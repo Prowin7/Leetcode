9b79449a71500b212beb5bacb8abf69c22306e32
Prowin7 <iec2022118@iiita.ac.in>
2026-01-20 18:36:30 +0530

    Time: 0 ms (100%), Space: 14 MB (77.79%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -4,16 +4,18 @@ public:
         int n=nums.size();
         int lo=0;
         int hi=n-1;
-        while(hi-lo>1){
+
+        while(lo<hi){
             int mid=lo+(hi-lo)/2;
             if(nums[mid]>nums[hi]){
-                lo=mid;
+                lo=mid+1;
             }
             else{
                 hi=mid;
             }
         }
-        if(nums[lo]<nums[hi]) return nums[lo];
-        else return nums[hi];
+
+        
+        return nums[lo];
     }
 };
